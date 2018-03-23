bb3af2d65a1460a26fefae0cd422f591a9c39a6d
rcs784 <rcstinson@hotmail.com>
2018-03-23 14:18:10 -0400

    sprint challenge complete

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,6 +5,16 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  const user = new User({ username, password });
+  User.save(user, (err, savedUser) => {
+    if (err) {
+      res.status(500);
+      res.json({ error: 'Error saving new user'} );
+      return;
+    }
+    res.status(200);
+    res.json(savedUser);
+  });
 };
 
 module.exports = {
