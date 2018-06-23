6d53471bb32d60f04e809b091d190de2892b2e2e
Victor Montoya <victoramontoya@gmail.com>
2018-06-23 13:56:30 -0700

    got stuck but nearly done

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,11 +1,18 @@
 const User = require('../models/userModels');
 const bcrypt = require('bcrypt');
+const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 
-const createUser = (req, res) => {
+
+router.post('/') , createUser = (req, res) => {
   const { username, password } = req.body;
+    User.create(req.body)
+    .then((user ) => res.status(201).json({ msg: 'user created!', username }))
+    .catch(err => res.status(500).send(err))
+
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
-};
+    };
 
 module.exports = {
   createUser
