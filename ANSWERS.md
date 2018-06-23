3c03548283630042b6da164c7b3bb8c1862a5926
unknown <alexisreyes90@gmail.com>
2018-06-23 14:45:38 -0400

    MVP functional/ made slight changes to code

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,7 +1,16 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
 
 1.  Describe Middleware, Sessions (as we know them in express), bcrypt and JWT.
+*Middleware adds more functionality, or extends what we do, but to have more control in what we do to the data. In our case we added an ```Authenticate``` middleware to see if the token we are given is in fact a correct token.
+
+*Bcrypt
+bcrypt will hash, or encrypt whatever value you have given it. For now we use Strings so it will hash a string however many SALT_ROUNDS we tell it to has
+
+*JWT/ Sessions
+Almost like sessions, where sessions will pass a cookie to the browser(only making it temporary) to access; in our case stored data. JWT acts exactly the same way, giving us a token instead of a cookie. 
 
 2.  What does bcrypt do in order to prevent attacks?
+Bcrypt lets us hash/ encrypt data.
 
 3.  What are the three parts of the JSON Web Token?
+header (Authorization in our case). key(payload). signtature(```jwt.sing(payload,SECRET,options)```)
--- api/models/userModels.js
+++ api/models/userModels.js
@@ -7,13 +7,33 @@ const SALT_ROUNDS = 11;
 const UserSchema = Schema({
   // create your user schema here.
   // username: required, unique and lowercase
+
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true
+  },
   // password: required
+
+  password: {
+    type: String,
+    required: true
+  }
 });
 
 UserSchema.pre('save', function(next) {
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
+  bcrypt.hash(this.password, SALT_ROUNDS, (err, hash) => {
+    if(err) {
+      return next(err);
+    }
+
+    this.password = hash;
+    next();
+  })
   // Once the password is encrypted, call next() so that your userController and create a user
 });
 
@@ -21,6 +41,16 @@ UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
+
+  return bcrypt.compare(plainTextPW, this.password, callBack)
+    // .then(response => {
+    //   callBack(null, response)
+    // })
+    // .catch(err => {
+    //   console.log(err)
+    // })
+
+
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
 };
 
