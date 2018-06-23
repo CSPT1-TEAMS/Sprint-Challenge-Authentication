be9fc4d46563e224588257eba5b1c545a63948fc
Alexander <alexander.botello@g.austincc.edu>
2018-06-23 12:32:32 -0500

    Complete createUser route

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,8 +3,13 @@ const bcrypt = require('bcrypt');
 
 const createUser = (req, res) => {
   const { username, password } = req.body;
-  // create user takes in the username and password and saves a user.
-  // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  User.create(req.body)
+    .then(({ username }) => {
+      res.status(201).json({ username });
+    })
+    .catch(err => {
+      res.status(500).json({msg: "Internal Server Error"})
+    })
 };
 
 module.exports = {
