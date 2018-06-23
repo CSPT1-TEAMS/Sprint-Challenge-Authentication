c770172f2292b90381c4e8e36059081f51bf7bf1
JPEmery <clearkeyemery@gmail.com>
2018-06-23 11:16:56 -0500

    JP Emery: Initial commit

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -7,10 +7,31 @@ const SALT_ROUNDS = 11;
 const UserSchema = Schema({
   // create your user schema here.
   // username: required, unique and lowercase
+  "username": {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  "password": {
+    type: String,
+    unique: true,
+    required: true,
+  }
   // password: required
 });
 
 UserSchema.pre('save', function(next) {
+
+  return bcrypt.hash(this.password, 11)
+  .then(hash => {
+    this.password = hash;
+    return next()
+  })
+  .catch(error => {
+    return next(err)
+  })
+
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // if there is an error here you'll need to handle it by calling next(err);
@@ -18,6 +39,10 @@ UserSchema.pre('save', function(next) {
 });
 
 UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
+
+  let TrueValue = bcrypt.compare(plainTextPW, this.password);
+  callBack(TrueValue);
+
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
