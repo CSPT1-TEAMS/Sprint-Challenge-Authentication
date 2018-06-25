c38e390c65904dea730afc6a7481183320adf218
Glenn-David Daniel <d2rd@outlook.com>
2018-06-24 22:45:06 -0700

    WIP crashes throws error OverwriteModelError: Cannot overwrite  model once compiled.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,12 +1,15 @@
-const User = require('../models/userModels');
-const bcrypt = require('bcrypt');
+const User = require('../models/userModels')
+const bcrypt = require('bcrypt')
+const { userSchema } = require('../models/usermodels')
 
+// create user takes in the username and password and saves a user.
 const createUser = (req, res) => {
-  const { username, password } = req.body;
-  // create user takes in the username and password and saves a user.
+  const { username, password } = req.body
+
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
-};
+  return ({ userSchema }.pre, username, password)
+}
 
 module.exports = {
   createUser
-};
+}
