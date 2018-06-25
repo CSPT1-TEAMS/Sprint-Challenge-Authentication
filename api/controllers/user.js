397bd1f2e4ae24a9e9de8faeb39973252b5793e7
Charlie <ccariste@gmail.com>
2018-06-24 22:26:37 -0700

    CRC: can add user to DB

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,10 +3,18 @@ const bcrypt = require('bcrypt');
 
 const createUser = (req, res) => {
   const { username, password } = req.body;
-  // create user takes in the username and password and saves a user.
-  // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  User.create(req.body)
+  .then(({ username }) => {
+    res.status(201).json({ username });
+  })
+  .catch(err => res.status(500).json({msg: 'Internal Server Error', err}));
 };
 
 module.exports = {
   createUser
 };
+
+// - Most of the heavy lifting will be taken care of in our pre save `middleware` that we've already implemented in our `userModel` file.
+// - Be sure to follow instructions on creating a user and you should be set
+// - **TEST** your `/api/users` _POST_ to ensure you can create a user with an encrypted password.
+// - Before moving on make sure you can create a user in the DB with an encrypted pw.
