6f3e8a2a109d13867de4f84195ded4b778ec2f7d
Glenn-David Daniel <d2rd@outlook.com>
2018-06-25 15:08:37 -0700

    new user typo correct.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -7,7 +7,7 @@ const createUser = (req, res) => {
   const { username, password } = req.body
 
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
-  const user = new User({user, password})
+  const user = new User({username, password})
   user.save()
     .then((result) => {
       res.status(200).send(result)
