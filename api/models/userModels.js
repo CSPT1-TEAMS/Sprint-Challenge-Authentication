fd5ffcbb696953a3813f24a81c1c4dddbe1e6b71
Andrew Jarrett <ahrjarrett@gmail.com>
2018-11-15 22:39:46 -0600

    create userModel, still needs to be tested; user controller next

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -1,27 +1,49 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const Schema = mongoose.Schema
 
-const SALT_ROUNDS = 11;
+const SALT_ROUNDS = 11
 
 const UserSchema = Schema({
   // create your user schema here.
   // username: required, unique and lowercase
   // password: required
-});
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  }
+})
 
-UserSchema.pre('save', function(next) {
+UserSchema.pre('save', (next) => {
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this middleware in with the Proper password encrypting, bcrypt.hash()
-  // if there is an error here you'll need to handle it by calling next(err);
+  // if there is an error here you'll need to handle it by calling next(err)
   // Once the password is encrypted, call next() so that your userController and create a user
-});
+  bcrypt.genSalt(SALT_ROUNDS, (err, salt) => {
+    bcrypt.hash(this.password, salt)
+      .then(hash => {
+        if (err) throw new Error(err)
+        else this.password = hash
+        return next()
+      })
+      .catch(err => next(err))
+  })
+})
 
-UserSchema.methods.checkPassword = function(plainTextPW, callBack) {
+UserSchema.methods.checkPassword = function (plainTextPW, cb) {
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // Fill this method in with the Proper password comparing, bcrypt.compare()
   // Your controller will be responsible for sending the information here for password comparison
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
-};
+  bcrypt.compare(plainTextPW, this.password)
+    .then(match => cb(match))
+    .catch(mismatch => console.log(mismatch))
+}
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema)
